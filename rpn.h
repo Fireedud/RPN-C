3bec312ff99b3402f77d83faba6dc5549a9fb573
Aman Grewal <aman1221@aol.com>
2013-09-17 11:15:15 -0400

    Changed it to have stacks now

--- rpn.h
+++ rpn.h
@@ -1,3 +1,11 @@
-typedef (*twoargs)(void, void, char []);
-typedef (*onearg)(void, char []);
+struct Stack_value{
+	int value;
+	Stack_value* prev;
+};
 
+struct Stack{
+	Stack_value* top;
+}
+
+int pop(Stack);
+void push(Stack, int);
