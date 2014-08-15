88063fd72677ba597641672770f5163bd419cba9
Aman Grewal <aman1221@aol.com>
2014-08-14 21:18:52 -0400

    Rpn works for addition and subtraction.

--- rpn.c
+++ rpn.c
@@ -1,38 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 #include "rpn.h"
 
-//Stack will be a global variable for this program
-
-void push(temp)
+int pop(struct Stack *S)
 {
+	assert(S->top != NULL);
+	struct Stack_value *top = S->top;
+	int retval = top->value;
+	(S->top) = top->prev;
+	free(top);
+	return retval;
 }
 
-void eval(char exp[])
+void push(struct Stack *S, int n)
 {
-	char c = '1'; //anything besides '\0' will work
-	int i = 0;
-	char temp[10]; //I will assume that for now
-	while(c != '\0'){
-		for(i=0; c != ' '; c = exp[i++]){
-			temp[i] = c;
+	struct Stack_value *val = malloc(sizeof(struct Stack_value));
+	assert(val != NULL);
+	(val->prev) = (S->top);
+	(val->value) = n;
+	(S->top) = val;
+}
+
+int eval(struct Stack *S, char exp[])
+{
+	char *val = strtok(exp, " ");
+	while(val != NULL){
+		if((strcmp(val,"+") != 0) && (strcmp(val,"-") != 0)){
+			push(S, atoi(val));
+		} else {
+			int n = pop(S);
+			int m = pop(S);
+			int r;
+			switch(val[0]){
+				case '+':
+					r = m+n;
+					break;
+				case '-':
+					r = m-n;
+					break;
+				default:
+					break;
+			}
+			push(S, r);
 		}
-		//PSUEDOCODE:
-		//If int(temp) {
-		//    push(temp);
-		//    }
-		//Else doop
+		val = strtok(NULL, " ");
 	}
+	return pop(S);
 }
 
 int main(int argc, char *argv[])
 {
 	char exp[200]; //I will assume this for now
+	struct Stack *S = calloc(1, sizeof(struct Stack));
 	while(1){
-		scanf("%s", &exp);
-		exp[199] = '\0'; //just in case
-		eval(exp);
-		printf("%i", Stack.top.value);
+		if(fgets(exp, 200, stdin) != NULL){
+			if(exp[0] == '\n') break;
+			//eliminate trailing newlines
+			int pos = strlen(exp) - 1; 
+			if(exp[pos] == '\n') exp[pos] = '\0';
+			printf("%i\n", eval(S, exp));
+		}
 	}
 
 	return 0;
--- rpn.h
+++ rpn.h
@@ -1,11 +1,11 @@
 struct Stack_value{
 	int value;
-	Stack_value* prev;
+	struct Stack_value* prev;
 };
 
 struct Stack{
-	Stack_value* top;
-}
+	struct Stack_value* top;
+};
 
-int pop(Stack);
-void push(Stack, int);
+int pop(struct Stack*);
+void push(struct Stack*, int);
